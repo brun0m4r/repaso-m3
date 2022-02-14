2ddcbd64c969ab065189873fc001715212fcd40c
YOLY <brunomarinich@gmail.com>
2022-02-14 17:57:23 -0300

    second commit

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ const server = express();
 
 server.use(express.json());
 
-server.listen(3001, () => {
-    console.log('running on port', 3001);
+server.listen(3000, () => {
+    console.log('running on port', 3000);
 });
