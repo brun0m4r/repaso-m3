54549c3fd931f8b07d9af9580f6e916fb28f9d16
YOLY <brunomarinich@gmail.com>
2022-02-14 17:53:36 -0300

    solutionn

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ server.use(express.json());
 server.use('/characters', characters);
 server.use('/species', species);
 
-server.listen(3001, () => {
-    console.log('running on port', 3001);
+server.listen(3000, () => {
+    console.log('running on port', 3000);
 });
